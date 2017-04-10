8619af2f52a4809a75ed6ef2d2b24bab393fd047
Dudás Ádám <sir.dudas.adam@gmail.com>
2017-04-10 22:02:38 +0200

    enhancements & refactoring

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const bodyParser = require('body-parser');
 const config = require('./config');
 const express = require('express');
 const nooocl = require('nooocl');
 const R = require('ramda');
 const ref = require('ref');
+
+const app = express();
+
+app.use(express.static('static'));
+
+app.use(bodyParser.json());
+
+app.options('/geometry.json', function (req, res) {
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.sendStatus(200);
+});
+
+
 
 const host = new nooocl.CLHost(config.version);
 
@@ -11,16 +27,28 @@ console.log(`Platform selected:`, platform.name);
 
 const devices = platform.allDevices();
 
-const context = new nooocl.CLContext(devices);
+const device = devices[0];
+console.log(`Device selected:`, device.name);
 
-const commandQueues = devices.map(device => new nooocl.CLCommandQueue(context, device));
+const context = new nooocl.CLContext(device);
+
+const commandQueue = new nooocl.CLCommandQueue(context, device);
 
 const commonSource = `
 size_t getPointIndex(size_t u, size_t v, size_t stride) {
     return (u % stride) + (v * stride);
 }
 `
-const getSurfacePointsSource = `
+function getDynamicProgramSource({expr}) {
+    return `
+float getRho(float phi, float z) {
+    return ${expr || '0.0f'};
+}
+
+float3 toCartesian(float rho, float phi, float z) {
+    return (float3)(cos(phi) * rho, sin(phi) * rho, z);
+}
+
 struct Params {
     float phiMin;
     float phiStep;
@@ -28,21 +56,12 @@ struct Params {
     float zStep;
 };
 
-float getRho(float phi, float z) {
-    return fmin(fmax(sin((z + phi) * 2.0f), -0.7f), 0.7f) + 3.0f;
-}
-
-float3 toCartesian(float rho, float phi, float z) {
-    return (float3)(cos(phi) * rho, sin(phi) * rho, z);
-}
-
 float3 getSurfacePoint(size_t u, size_t v, __constant struct Params *params) {
     float phi = params->phiMin + u * params->phiStep;
     float z = params->zMin + v * params->zStep;
     return toCartesian(getRho(phi, z), phi, z);
 }
-`
-const dynamicProgramSource = getSurfacePointsSource + commonSource + `
+${commonSource}
 __kernel void computePoints(__global float *points, __constant struct Params *params) {
     size_t stride = get_global_size(0);
     size_t u = get_global_id(0);
@@ -50,7 +69,8 @@ __kernel void computePoints(__global float *points, __constant struct Params *pa
 
     vstore3(getSurfacePoint(u, v, params), getPointIndex(u, v, stride), points);
 }
-`
+`;
+}
 const staticProgramSource = commonSource + `
 size_t getQuadIndex(size_t u, size_t v, size_t stride) {
     return (u % stride) + (v * stride);
@@ -99,9 +119,12 @@ function buildProgram(program) {
 
 const isError = (buildResult) => buildResult.status === host.cl.defs.CL_BUILD_ERROR;
 
-function logBuildResult(buildResult) {
-    console.log(`Build on ${buildResult.device.name}: ${isError(buildResult) ? '✗' : '✓'}`);
-    if (isError(buildResult)) console.log(buildResult.log);
+function logBuildResults(buildResults) {
+    console.info(`Build results:`);
+    buildResults.forEach(buildResult => {
+        console.info(`Build on ${buildResult.device.name}: ${isError(buildResult) ? '✗' : '✓'}`);
+        if (isError(buildResult)) console.log(buildResult.log);
+    });
 }
 
 function setBuffer(buffer, ...data) {
@@ -113,84 +136,91 @@ function setBuffer(buffer, ...data) {
     return buffer;
 }
 
-function getGeometry() {
-    var z_min = 0.0, z_max = 10.0, z_res = 50;
-    var phi_min = 0.0, phi_max = 2.0*Math.PI, phi_res = 60;
-
-    var z_step = (z_max - z_min) / z_res;
-    var phi_step = (phi_max - phi_min) / phi_res;
-
-    const staticProgram = context.createProgram(staticProgramSource);
-    const dynamicProgram = context.createProgram(dynamicProgramSource);
-    return Promise.all([
-        buildProgram(staticProgram).then(res => (console.log(`Static program built.`), res)),
-        buildProgram(dynamicProgram).then(res => (console.log(`Dynamic program built.`), res))
-    ]).then(([staticBuildResults, dynamicBuildResults]) => {
-        if (staticBuildResults.some(isError) || dynamicBuildResults.some(isError)) {
-            console.error('Some build(s) failed.');
-            console.info('Static program results:');
-            staticBuildResults.forEach(logBuildResult);
-            console.info('Dynamic program results:');
-            dynamicBuildResults.forEach(logBuildResult);
-        } else {
-            const computeMeshKernel = staticProgram.createKernel('computeMesh');
-            const computePointsKernel = dynamicProgram.createKernel('computePoints');
-
-            const params = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_READ_ONLY, ref.types.float.size * 4);
-            const paramsBuffer = setBuffer(Buffer.alloc(ref.types.float.size * 4), ...[phi_min, phi_step, z_min, z_step].map(value => ({ type: ref.types.float, value})));
-
-            const numOfPoints = (z_res + 1) * phi_res;
-            const numOfVertices = z_res * phi_res * 2 * 3;
-            const points = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_READ_WRITE, ref.types.float.size * 3 * numOfPoints);
-            const vertices = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_WRITE_ONLY, ref.types.float.size * 3 * numOfVertices);
-            const vertexBuffer = Buffer.alloc(vertices.size);
-            const normals = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_WRITE_ONLY, ref.types.float.size * 3 * numOfVertices);
-            const normalBuffer = Buffer.alloc(normals.size);
-            console.log(`Buffers allocated.`, points.size, vertices.size, normals.size);
-
-            computePointsKernel.setArgs(points, params);
-            computeMeshKernel.setArgs(points, vertices, normals);
-            console.log(`Kernel args set.`);
-            
-            return commandQueues[0].waitable().enqueueWriteBuffer(params, 0, params.size, paramsBuffer).promise.then(() => {
-                console.log(`Params buffer write done.`);
-                return commandQueues[0].waitable().enqueueNDRangeKernel(computePointsKernel, new nooocl.NDRange(phi_res, z_res + 1)).promise;
-            }).then(() => {
-                console.log(`Points computed.`);
-                return commandQueues[0].waitable().enqueueNDRangeKernel(computeMeshKernel, new nooocl.NDRange(phi_res, z_res, 2)).promise;
-            }).then(() => {
-                console.log(`Mesh computed.`);
-                return commandQueues[0].waitable().enqueueReadBuffer(vertices, 0, vertices.size, vertexBuffer).promise;
-            }).then(() => {
-                console.log(`Vertices read.`);
-                return commandQueues[0].waitable().enqueueReadBuffer(normals, 0, normals.size, normalBuffer).promise;
-            }).then(() => {
-                console.log(`Normals read.`);
-                return ({
-                    vertices: new Array(numOfVertices * 3).fill(null).map((v, i) => vertexBuffer.readFloatLE(ref.types.float.size * i)),
-                    normals: new Array(numOfVertices * 3).fill(null).map((v, i) => normalBuffer.readFloatLE(ref.types.float.size * i)),
-                });
-            });
-        }
-    });
+function getStep({min, max, res}) {
+    return (max - min) / res;
 }
 
-const app = express();
+function readFloats(buffer) {
+    if (buffer.length % ref.types.float.size !== 0) console.warn(`Buffer size not a multiple of float size.`);
+    return new Array(buffer.length / ref.types.float.size).fill(null).map((v, i) => buffer.readFloatLE(ref.types.float.size * i));
+}
 
-//app.use(express.static('.'));
+const staticProgram = context.createProgram(staticProgramSource);
+buildProgram(staticProgram).then(staticBuildResults => {
+    console.log(`Static program build finished.`);
+    if (staticBuildResults.some(isError)) {
+        console.error(`Some build(s) failed.`);
+        logBuildResults(staticBuildResults);
+        throw new Error(`Static program build failure.`);
+    } else {
+        function getGeometry({phi, z, expr}) {
+            var z_step = getStep(z);
+            var phi_step = getStep(phi);
 
-app.options('/geometry.json', function (req, res) {
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    res.set('Access-Control-Allow-Methods', 'GET');
-    res.set('Access-Control-Allow-Origin', '*');
-    res.sendStatus(200);
+            const dynamicProgram = context.createProgram(getDynamicProgramSource({expr}));
+            return buildProgram(dynamicProgram).then(dynamicBuildResults => {
+                console.log(`Dynamic program build finished.`);
+                if (dynamicBuildResults.some(isError)) {
+                    console.error(`Some build(s) failed.`);
+                    logBuildResults(dynamicBuildResults);
+                } else {
+                    const numOfPoints = (z.res + 1) * phi.res;
+                    const points = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_READ_WRITE, ref.types.float.size * 3 * numOfPoints);
+
+                    const numOfVertices = z.res * phi.res * 2 * 3;
+                    const vertices = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_WRITE_ONLY, ref.types.float.size * 3 * numOfVertices);
+                    const vertexBuffer = Buffer.alloc(vertices.size);
+                    const normals = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_WRITE_ONLY, ref.types.float.size * 3 * numOfVertices);
+                    const normalBuffer = Buffer.alloc(normals.size);
+                    console.log(`Buffers allocated.`);
+
+                    const paramsBuffer = setBuffer(Buffer.alloc(ref.types.float.size * 4), ...[phi.min, phi_step, z.min, z_step].map(value => ({ type: ref.types.float, value})));
+                    const params = new nooocl.CLBuffer(context, host.cl.defs.CL_MEM_READ_ONLY, ref.types.float.size * 4);
+                    return commandQueue.waitable().enqueueWriteBuffer(params, 0, params.size, paramsBuffer).promise.then(() => {
+                        console.log(`Params buffer write done.`);
+                        const computePointsKernel = dynamicProgram.createKernel('computePoints');
+                        computePointsKernel.setArgs(points, params);
+                        return commandQueue.waitable().enqueueNDRangeKernel(computePointsKernel, new nooocl.NDRange(phi.res, z.res + 1)).promise.then(() => {
+                            console.log(`Points computed.`);
+                            computePointsKernel.release();
+                            params.release();
+                        });
+                    }).then(() => {
+                        const computeMeshKernel = staticProgram.createKernel('computeMesh');
+                        computeMeshKernel.setArgs(points, vertices, normals);
+                        return commandQueue.waitable().enqueueNDRangeKernel(computeMeshKernel, new nooocl.NDRange(phi.res, z.res, 2)).promise.then(() => {
+                            console.log(`Mesh computed.`);
+                            computeMeshKernel.release();
+                            points.release();
+                        });
+                    }).then(() => commandQueue.waitable().enqueueReadBuffer(vertices, 0, vertices.size, vertexBuffer).promise.then(() => {
+                        console.log(`Vertices read.`);
+                        vertices.release();
+                    })).then(() => commandQueue.waitable().enqueueReadBuffer(normals, 0, normals.size, normalBuffer).promise.then(() => {
+                        console.log(`Normals read.`);
+                        normals.release();
+                    })).then(() => ({
+                        vertices: readFloats(vertexBuffer),
+                        normals: readFloats(normalBuffer),
+                    }));
+                }
+            });
+        }
+
+        app.post('/geometry.json', function (req, res) {
+            res.set('Access-Control-Allow-Origin', '*');
+            res.set('Content-Disposition', 'attachment; filename="geometry.json"');
+            getGeometry(req.body).then(geometry => res.json(geometry));
+        });
+
+        app.listen(config.port, function () {
+            console.log(`Listening on http://localhost:${config.port}...`);
+        })
+
+    }
+}).catch(err => {
+    console.error(err);
+    process.exit(1);
 });
-app.get('/geometry.json', function (req, res) {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Content-Disposition', 'attachment; filename="geometry.json"');
-    getGeometry().then(geometry => res.json(geometry));
-});
 
-app.listen(config.port, function () {
-    console.log(`Listening on http://localhost:${config.port}...`);
-})
+
---
+++ static/index.js
@@ -0,0 +1,201 @@
+var DEBUG = true;
+
+vec2.angle = function(v) {
+    return Math.atan2(v[1], v[0]);
+};
+
+mat4.uniformScale = function(out, m, s) {
+    return mat4.scale(out, m, vec3.fromValues(s, s, s));
+};
+
+function updateCanvasBackBufferSize(gl, scale=1) {
+    var canvas = gl.canvas;
+    var width = canvas.clientWidth / scale;
+    var height = canvas.clientHeight / scale;
+
+    if (canvas.width != width || canvas.height != height) {
+        canvas.width = width;
+        canvas.height = height;
+        gl.viewport(0, 0, width, height);
+    }
+}
+
+function createGL(canvas) {
+    return canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+}
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        return shader;
+    } else {
+        console.error(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+    }
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        return program;
+    } else {
+        console.error(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    }
+}
+
+function clamp(min, val, max) {
+    return Math.max(min, Math.min(max, val));
+}
+
+function getGeometry(params) {
+    return fetch(`/geometry.json`, {
+        body: JSON.stringify(params),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        mode: 'cors',
+    }).then(response => response.json());
+}
+
+function init() {
+    var canvas = document.querySelector('canvas');
+    var vertexShaderSource = document.getElementById('vertex-shader').innerText;
+    var fragmentShaderSource = document.getElementById('fragment-shader').innerText;
+    var bgColor = [1, 1, 1, 1];
+
+    var gl = createGL(canvas);
+    if (DEBUG) window.gl = gl;
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LESS);
+
+    updateCanvasBackBufferSize(gl);
+
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    var program = createProgram(gl, vertexShader, fragmentShader);
+    gl.useProgram(program);
+
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    var normalAttributeLocation = gl.getAttribLocation(program, "a_normal");
+    gl.enableVertexAttribArray(normalAttributeLocation);
+
+    var modelMatrix = mat4.create();
+    var viewMatrix = mat4.fromTranslation([], vec3.fromValues(0, 0, -1));
+    var projectionMatrix = mat4.perspective([], Math.PI / 3, canvas.width / canvas.height, 0.01, 100);
+
+    var pvmMatrixLocation = gl.getUniformLocation(program, "u_pvmMatrix");
+    var vmitMatrixLocation = gl.getUniformLocation(program, "u_vmitMatrix");
+
+    var coordVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, coordVertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1]), gl.STATIC_DRAW);
+
+    var vertexBuffer = gl.createBuffer();
+    var normalBuffer = gl.createBuffer();
+
+    var draw = function() {};
+
+    function loadGeometry(geometry) {
+        if (DEBUG) window.geometry = geometry;
+
+        if (geometry.normals.length !== geometry.vertices.length) console.error('|ns| != |vs|');
+        if (geometry.vertices.length % 3 !== 0) console.error('|vs| % 3 != 0');
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry.vertices), gl.STATIC_DRAW);
+        gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(geometry.normals), gl.STATIC_DRAW);
+        gl.vertexAttribPointer(normalAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+        return geometry;
+    }
+
+    window.addEventListener('resize', function() {
+        updateCanvasBackBufferSize(gl);
+        projectionMatrix = mat4.perspective([], Math.PI / 3, canvas.width / canvas.height, 0.01, 100);
+        draw();
+    });
+
+    gl.clearColor(...bgColor);
+
+    canvas.addEventListener('mousemove', function (e) {
+        var scaleFactor = 2;
+        var sphereSize = 0.7;
+        if (e.buttons === 1) {
+            var endV = vec2.fromValues(
+                2.0 * e.clientX / e.target.clientWidth - 1.0,
+                -2.0 * e.clientY / e.target.clientHeight + 1.0
+            );
+            var startV = vec2.fromValues(
+                2.0 * (e.clientX - e.movementX) / e.target.clientWidth - 1.0,
+                -2.0 * (e.clientY - e.movementY) / e.target.clientHeight + 1.0
+            );
+            var startL = vec2.length(startV);
+            if (startL < sphereSize) {
+                var startVL = vec2.scale([], startV, 1 / sphereSize);
+                var startD = vec3.fromValues(startVL[0], startVL[1], Math.cos(Math.asin(clamp(-1.0, vec2.length(startVL), 1.0))));
+                var endVL = vec2.scale([], endV, 1 / sphereSize);
+                var endD = vec3.fromValues(endVL[0], endVL[1], Math.cos(Math.asin(clamp(-1.0, vec2.length(endVL), 1.0))));
+                var axis = [];
+                var angle = quat.getAxisAngle(axis, quat.rotationTo([], startD, endD));
+                vec3.transformMat4(axis, axis, mat4.invert([], modelMatrix));
+                mat4.rotate(modelMatrix, modelMatrix, angle, axis);
+            } else {
+                var radial = vec2.length(endV) - startL;
+                var angle = vec2.angle(endV) - vec2.angle(startV);
+                mat4.rotate(modelMatrix, modelMatrix, angle, vec3.transformMat4([], vec3.fromValues(0, 0, 1), mat4.invert([], modelMatrix)));
+                mat4.uniformScale(viewMatrix, viewMatrix, 1.0 + radial * scaleFactor);
+            }
+            draw();
+        }
+    });
+
+    document.querySelectorAll('input[name="primitive-type"]').forEach(elem => elem.addEventListener('change', () => {
+        draw();
+    }));
+
+    document.getElementById('submit').addEventListener('click', function() {
+        getGeometry({
+            phi: {
+                min: parseFloat(document.getElementById('phi-min').value),
+                max: parseFloat(document.getElementById('phi-max').value),
+                res: parseInt(document.getElementById('phi-res').value)
+            },
+            z: {
+                min: parseFloat(document.getElementById('z-min').value),
+                max: parseFloat(document.getElementById('z-max').value),
+                res: parseInt(document.getElementById('z-res').value)
+            },
+            expr: document.getElementById('expr').value,
+        }).then(loadGeometry).then(function (geometry) {
+            var size = R.splitEvery(3, geometry.vertices).map(v => vec3.length(v)).reduce(R.max, 0);
+            viewMatrix = mat4.uniformScale([], mat4.fromTranslation([], vec3.fromValues(0, 0, -1)), 0.5 / size);
+            draw = function() {
+                var viewModelMatrix = mat4.mul([], viewMatrix, modelMatrix);
+                gl.uniformMatrix4fv(pvmMatrixLocation, false, mat4.mul([], projectionMatrix, viewModelMatrix));
+                gl.uniformMatrix4fv(vmitMatrixLocation, false, mat4.transpose([], mat4.invert([], viewModelMatrix)));
+                gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+                var primitiveType = [...document.querySelectorAll('input[name="primitive-type"]')].find(elem => elem.checked).value;
+                gl.drawArrays(gl[primitiveType], 0, geometry.vertices.length / 3);
+            }
+
+            draw();
+        }, err => console.log(err));
+    });
+    document.getElementById('reset-view-matrix').addEventListener('click', function() {
+        viewMatrix = mat4.uniformScale([], mat4.fromTranslation([], vec3.fromValues(0, 0, -1)), 0.1);
+        draw();
+    });
+}
